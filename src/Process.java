4110724ca4d14f79857ad8c5fade790dc0da601f
SaraAlajmy <saraalagami@gmail.com>
2023-05-20 02:19:41 +0300

    our work

--- src/Process.java
+++ src/Process.java
@@ -1,9 +1,35 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+
 public class Process {
     private PCB info;
     public static int counter=1;
+    private int timeSlice = Integer.parseInt(getVal("slice"));
 
     public Process(int min,int max){
         info=new PCB(counter,min,max);
         counter++;
+    }
+
+    public static String getVal(String key)
+    {
+        String keyval = null;
+        //Let's consider properties file is in project folder itself
+
+        File file = new File("src/resources/os.config");
+
+        //Creating properties object
+        Properties prop = new Properties();
+        //Creating InputStream object to read data
+        FileInputStream objInput = null;
+        try{
+            objInput = new FileInputStream(file);
+            //Reading properties key/values in file
+            prop.load(objInput);
+            keyval = prop.getProperty(key);
+            objInput.close();
+        }catch(Exception e){System.out.println(e.getMessage());}
+        return keyval;
     }
 }
---
+++ src/Scheduler.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.LinkedList;
+import java.util.Properties;
+import java.util.Queue;
+
+public class Scheduler {
+
+    private Queue<Process> readyQueue = new LinkedList<>();
+    private Queue<Process> blockedQueue = new LinkedList<>();
+
+    public void choose(){
+
+    }
+
+    public void block(){
+
+    }
+
+    public void terminate(){
+
+    }
+
+    public void printQueue(Queue<Process> queue){
+        for()
+    }
+
+}
